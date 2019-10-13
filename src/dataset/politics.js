00c089ec433a152a6b757707c9caafc240d4e148
Ali İhsan Ofluoğlu <ali@atolye15.com>
2019-10-13 23:25:50 +0300

    create dataset

---
+++ src/dataset/politics.js
@@ -0,0 +1,6 @@
+const politics = [
+  'AK Parti’den istifa eden eski Başbakan Ahmet Davutoğlu 18 Ekim’de Manisada yapacağı konuşmayla yeni partiyi anlatacak ve yol haritasını açıklayacak',
+  'Davutoğlu’na yakın bir kaynaktan alınan bilgilere göre partinin bir ay içerisinde kurulması bekleniyor. Partisinin kurulma çalışmalarını hızlandıran Davutoğlu, hem İstanbul’daki hem de Ankara’daki ofisinde çalışmalarını yürütüyor.',
+];
+
+export default politics;
