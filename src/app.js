5e9d25c2ce22d56f2649443362807c3fd11789f5
Ali İhsan Ofluoğlu <ali@atolye15.com>
2019-10-14 23:55:54 +0300

    bayes for politics data

--- src/app.js
+++ src/app.js
@@ -1,4 +1,36 @@
 import politics from './dataset/politics';
+import database from './firebase/firebase';
 import getWordCount from './utils/getWordCount';
+import readPoliticsData from './api/politics.api';
 
-console.log(getWordCount(politics)); // yazdırma
+// const politicsWordCounts = getWordCount(politics);
+
+// database
+//   .collection('bitirme')
+//   .doc('politics')
+//   .set({
+//     politicsWordCounts,
+//   })
+//   .then(() => {
+//     console.log('Document successfully written!');
+//   })
+//   .catch(error => {
+//     console.error('Error writing document: ', error);
+//   });
+
+const input = 'ak parti bir partidir';
+
+readPoliticsData().then(result => {
+  result.forEach(a => {
+    const datas = a.data();
+
+    const parseInput = input.split(' ');
+
+    parseInput.forEach(parseWord => {
+      const rate =
+        datas.politicsWordCounts[parseWord] / Object.keys(datas.politicsWordCounts).length;
+
+      console.log(parseWord + ' ' + rate / 4);
+    });
+  });
+});
