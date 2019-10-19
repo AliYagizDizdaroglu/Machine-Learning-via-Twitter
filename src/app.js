247d29a2ebb1cab10464219ef1fff2db49a296c7
Ali İhsan Ofluoğlu <ali@atolye15.com>
2019-10-19 21:53:15 +0300

    create bayes alg

--- src/app.js
+++ src/app.js
@@ -5,15 +5,20 @@
 // import music from './dataset/music';
 // import database from './firebase/firebase';
 // import getWordCount from './utils/getWordCount';
-// import readPoliticsData from './api/politics.api';
+import firebase from 'firebase';
+import config from './firebase/config';
 
-// const politicsWordCounts = getWordCount(politics);
+firebase.initializeApp(config);
+
+const database = firebase.firestore();
+
+// const data = getWordCount(music);
 
 // database
 //   .collection('bitirme')
-//   .doc('politics')
+//   .doc('music')
 //   .set({
-//     politicsWordCounts,
+//     data,
 //   })
 //   .then(() => {
 //     console.log('Document successfully written!');
@@ -21,3 +26,51 @@
 //   .catch(error => {
 //     console.error('Error writing document: ', error);
 //   });
+
+Object.size = obj => {
+  let size = 0;
+  let key;
+  // eslint-disable-next-line no-restricted-syntax
+  for (key in obj) {
+    // eslint-disable-next-line no-prototype-builtins
+    if (obj.hasOwnProperty(key)) size += 1;
+  }
+  return size;
+};
+
+const input =
+  'Usülsüz ihale oyunu Haydarpaşa İstanbul halkına işadamlarına sermaye peşkeş çekmek isteyenler';
+
+const DATASETNAME = ['politics', 'music', 'economy', 'sport', 'science'];
+const databaseData = [];
+
+DATASETNAME.forEach(datasetName => {
+  database
+    .collection('bitirme')
+    .doc(datasetName)
+    .get()
+    .then(datas => {
+      if (!datas.exists) return;
+
+      const realData = datas.data().data;
+
+      const testData = input.split(' ');
+      let rate = 0;
+
+      testData.forEach(word => {
+        console.log(`${datasetName} ${realData[word]}`);
+        rate +=
+          parseFloat(realData[word] === undefined ? 0 : realData[word]) /
+          parseFloat(Object.size(realData));
+      });
+
+      const element = {};
+
+      element.dataset = datasetName;
+      element.value = rate / testData.length;
+
+      databaseData.push(element);
+    });
+});
+
+console.log(databaseData);
